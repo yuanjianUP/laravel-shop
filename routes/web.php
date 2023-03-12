e0f516e2d0c4c03926d23f073fb42dbf33e67916
leo <1587229752@qq.com>
2023-03-12 12:33:01 +0800

    收获地址列表

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => ['auth']],function() {
+      Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
+
