de16ce7b46b026cb5788e69d40cb5a3c973d5b9a
vagrant <vagrant@homestead>
2023-05-05 10:36:25 +0000

    后台问题

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## laravel练习
+### 如果没有对应后台账号数据
+php artisan admin:install
+
+### 创建后台账号
+php artisan admin:create-user
---
+++ app/Models/CouponCode.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Encore\Admin\Traits\DefaultDatetimeFormat;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CouponCode extends Model
+{
+    use HasFactory;
+    use DefaultDatetimeFormat;
+    
+    const TYPE_FIXED = 'fixed';
+    const TYPE_PERCENT = 'percent';
+
+    public static $typeMap = [
+        self::TYPE_FIXED => '固定金额',
+        self::TYPE_PERCENT => '比例',
+    ];
+
+    protected $fillable = [
+        'name',
+        'code',
+        'type',
+        'value',
+        'total',
+        'used',
+        'min_amount',
+        'not_before',
+        'not_after',
+        'enable',
+    ];
+
+    protected $casts = [
+        'enable' => 'boolean',
+    ];
+
+    
+
+}
